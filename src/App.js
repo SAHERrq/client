0cd4981b6b0909e60ba7b88f848ac70a9c2df81d
SAHERrq <asaher787@gmail.com>
2023-12-30 22:09:18 +0200

    123

--- src/App.js
+++ src/App.js
@@ -21,5 +21,27 @@ function App() {
     </div>
   );
 }
-
+// i copy paste this 
+class App extends Component {
+  state = { loading: true, drizzleState: null };
+componentDidMount() {
+    const { drizzle } = this.props;
+// subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+// every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+// check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+compomentWillUnmount() {
+    this.unsubscribe();
+  }
+render() {
+    if (this.state.loading) return "Loading Drizzle...";
+    return <div className="App">Drizzle is ready</div>;
+  }
+}
 export default App;
